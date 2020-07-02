3583a35cf0703a1b7cf38a5cc75bac3b5a3d2198
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-02 21:42:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introdcution_with_language(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to porgram in #{language}."
 end
 
