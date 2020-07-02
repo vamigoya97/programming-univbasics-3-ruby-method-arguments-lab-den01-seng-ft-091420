4309f18f08fe18f62658c763b6a34c3dd8ac1b5c
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-02 21:39:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,10 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+name = Victor
 def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
+language = Java
 def introdcution_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to porgram in #{language}"
 end
